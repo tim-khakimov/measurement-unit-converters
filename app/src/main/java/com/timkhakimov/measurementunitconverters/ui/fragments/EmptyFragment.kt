81ff58300f053871504173ccca29a59c32872bb0
Timur Khakimov <tkh1511@gmail.com>
2020-03-22 14:11:25 +0300

    [+] EmptyFragment - Фрагмент - заглушка

---
+++ app/src/main/java/com/timkhakimov/measurementunitconverters/ui/fragments/EmptyFragment.kt
@@ -0,0 +1,15 @@
+package com.timkhakimov.measurementunitconverters.ui.fragments
+
+import com.timkhakimov.measurementunitconverters.R
+import com.timkhakimov.measurementunitconverters.databinding.FragmentEmptyBinding
+
+/**
+ * Created by Timur Khakimov on 22.03.2020
+ * Фрагмент - заглушка
+ */
+class EmptyFragment : BaseFragment<FragmentEmptyBinding>() {
+
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_empty
+    }
+}
